f2375e61dc1c57ba3abb7ba44bdd587299cba82b
Kukuh <hi@kalfian.com>
2025-04-22 07:24:14 +0000

    add several apps

---
+++ radicale/infcloud/config.js
@@ -0,0 +1,8 @@
+var globalNetworkCheckSettings = {
+  href: 'http://localhost:5232/',
+  userAuth: true,
+  timeOut: 30000,
+  lockTimeOut: 10000,
+  checkContentType: true,
+  settingsAccount: true
+};
