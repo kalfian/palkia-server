a0fd65f5d1ca399ddcc3b2a1124dbf73e5bee6c2
Kukuh <hi@kalfian.com>
2025-05-02 08:57:41 +0000

    addignore

--- radicale/infcloud/config.js
+++
@@ -1,8 +0,0 @@
-var globalNetworkCheckSettings = {
-  href: 'http://localhost:5232/',
-  userAuth: true,
-  timeOut: 30000,
-  lockTimeOut: 10000,
-  checkContentType: true,
-  settingsAccount: true
-};
